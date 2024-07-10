5ddbd69b039f1bc91c5e5014622b3ecd0e546b91
reza1mahb <136892089+reza1mahb@users.noreply.github.com>
2024-07-10 08:21:07 +0330

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 address=0x5fc07e8E97aB660557Bc00628EB1bA14C4a8088F
 agents=$(./user-bot.sh agents --all -f FSimCoinX | grep -Eo ^[0-9a-zA-Z]{42})
 
-export RED='\033[0;31m'
+export RED='\8763[0;31m'
 export GREEN='\033[0;32m'
 export WHITE="\033[0;37m"
 export NC='\033[0m' # No Color
